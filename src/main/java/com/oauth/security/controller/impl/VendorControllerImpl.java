8c8a558c20675238aea54034b989a51200415bbc
Avinash Patel <patelavinash508@gmail.com>
2022-04-20 00:19:38 +0530

    base plate

---
+++ src/main/java/com/oauth/security/controller/impl/VendorControllerImpl.java
@@ -0,0 +1,10 @@
+package com.oauth.security.controller.impl;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.oauth.security.controller.VendorController;
+
+@RestController
+public class VendorControllerImpl implements VendorController {
+
+}
