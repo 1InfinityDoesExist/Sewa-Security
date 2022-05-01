8cd25a1f35a388ad16ff4d978265ec206592c9c6
Avinash Patel <patelavinash508@gmail.com>
2022-05-02 00:54:53 +0530

    Product apis completed

--- src/main/java/com/oauth/security/service/ProductService.java
+++ src/main/java/com/oauth/security/service/ProductService.java
@@ -1,13 +1,22 @@
 package com.oauth.security.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
-import com.oauth.security.model.request.ProductRequest;
+import com.oauth.security.entity.Product;
+import com.oauth.security.model.request.ProductCreateRequest;
 import com.oauth.security.model.response.ProductResponse;
 
 @Component
 public interface ProductService {
 
-	ProductResponse registerProductUsingPOST(ProductRequest productRequest);
+	ProductResponse registerProductUsingPOST(ProductCreateRequest productCreateRequest);
+
+	Product retrieveProductUsingGET(String name);
+
+	Page<Product> retrieveAllProductsUsingGET(Pageable pageable);
+
+	void deleteProductUsingDELETE(String name);
 
 }
--- src/main/java/com/oauth/security/service/impl/ProductServiceImpl.java
+++ src/main/java/com/oauth/security/service/impl/ProductServiceImpl.java
@@ -1,11 +1,13 @@
 package com.oauth.security.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 import com.oauth.security.entity.Product;
-import com.oauth.security.model.request.ProductRequest;
+import com.oauth.security.model.request.ProductCreateRequest;
 import com.oauth.security.model.response.ProductResponse;
 import com.oauth.security.repository.ProductRepository;
 import com.oauth.security.service.ProductService;
@@ -20,15 +22,15 @@ public class ProductServiceImpl implements ProductService {
 	private ProductRepository productRepository;
 
 	@Override
-	public ProductResponse registerProductUsingPOST(ProductRequest productRequest) {
+	public ProductResponse registerProductUsingPOST(ProductCreateRequest productCreateRequest) {
 		log.info("-----ProductServiceImpl Class, registerProductUsingPOST method.-----");
 
-		Product product = productRepository.findProductByName(productRequest.getName());
+		Product product = productRepository.findProductByNameAndIsActive(productCreateRequest.getName(), true);
 		if (ObjectUtils.isEmpty(product)) {
 			product = new Product();
 			product.setActive(true);
-			product.setName(productRequest.getName());
-			product.setRoles(productRequest.getRoles());
+			product.setName(productCreateRequest.getName());
+			product.setRoles(productCreateRequest.getRoles());
 			productRepository.save(product);
 
 			ProductResponse response = new ProductResponse();
@@ -36,7 +38,34 @@ public class ProductServiceImpl implements ProductService {
 			response.setMsg("Success");
 			return response;
 		} else {
-			throw new RuntimeException(String.format("Product with name : %s already exist", productRequest.getName()));
+			throw new RuntimeException(
+					String.format("Product with name : %s already exist", productCreateRequest.getName()));
 		}
+	}
+
+	@Override
+	public Product retrieveProductUsingGET(String name) {
+		log.info("-----ProductServiceImpl Class, retrieveProductUsingGET method.-----");
+		Product product = productRepository.findProductByNameAndIsActive(name, true);
+		if (ObjectUtils.isEmpty(product)) {
+			throw new RuntimeException(String.format("Product with name %s does not exit", name));
+		}
+		return product;
+	}
+
+	@Override
+	public Page<Product> retrieveAllProductsUsingGET(Pageable pageable) {
+		log.info("-----ProductServiceImpl Class, retrieveAllProductsUsingGET method.-----");
+
+		return productRepository.findProductByIsActive(true, pageable);
+	}
+
+	@Override
+	public void deleteProductUsingDELETE(String name) {
+		log.info("-----ProductServiceImpl Class, deleteProductUsingDELETE method.-----");
+		Product product = retrieveProductUsingGET(name);
+		product.setActive(false);
+		productRepository.save(product);
+		return;
 	}
 }
